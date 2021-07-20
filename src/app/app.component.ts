992f7cea0976b5c982220617329aa1f4fa196272
quincarter <quin.carter@gmail.com>
2021-07-19 21:12:43 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   date: string;
   moneyValue: string;
+  percentValue: string;
+  firstAndLastName: string;
   error: string;
+
+  // label for the title case input using the titlecase pipe
+  titleCaseStringExample =
+    'enter something here and it will always capitalize the first letter in each word';
 
   // Only checks to see if the date is blank -- that indicates an error from the pipe
   updateDate($event) {
